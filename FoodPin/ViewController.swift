7c675b6a37aed4a17dbf5a79e86d9db9b123ff41
Steven-Tang-rong <gzodc0045288@gmail.com>
2020-04-26 20:32:57 +0800

    Initial Commit

---
+++ FoodPin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodPin
+//
+//  Created by TANG,QI-RONG on 2020/4/26.
+//  Copyright © 2020 Steven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
